2d774130329c92de63d144311e79e4146bf18e2c
njm25 <nicomilette7@gmail.com>
2026-01-24 11:41:16 -0500

    feat: test slocal storage gneratin

--- index.js
+++ index.js
@@ -3,6 +3,20 @@
 window.addEventListener('DOMContentLoaded', () => {
     console.log('Page loaded and ready!');
     console.log(window.location.href);
-    localStorage.setItem('test', 'hello world');
+    localStorage.setItem(btoa(num.toString()), btoa((num + 1).toString()));
     sessionStorage.setItem('sessionTest', 'session hello world');        
+    // generateBs();
 });
+
+function generateBs(){
+    let i = 0;
+    while(true){
+
+
+        i = i + 2;
+    }
+}
+
+function generateB64FromInt(num){
+    return 
+}
