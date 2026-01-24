ebbedfa27a33f2f2fc7f29539c21e0d5a4351c85
njm25 <nicomilette7@gmail.com>
2026-01-23 23:29:28 -0500

    chore: local host testing

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+
+
 window.addEventListener('DOMContentLoaded', () => {
     console.log('Page loaded and ready!');
-    console.log(window.location.href);    
+    console.log(window.location.href);
+    localStorage.setItem('test', 'hello world');
+    sessionStorage.setItem('sessionTest', 'session hello world');        
 });
